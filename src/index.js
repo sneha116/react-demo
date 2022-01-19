30f9d78cd6303600f0647aaf2312b5ddce951377
sneha <snehaponnu116@gmail.com>
2022-01-19 13:56:30 +0530

    Task 1: Find the sum of two numbers.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import {useRef, useState} from "react";
 
-const myfirstelement = <h1>Hello React!</h1>
+function Sum() {
+  const num_1Ref = useRef(0);
+  const num_2Ref = useRef(0);
+  const [Sum, setSum] = useState(0);
 
-ReactDOM.render(myfirstelement, document.getElementById('root'));
+  //function to calculate the sum of numbers
+  const addNumbers = () => {
+    let no1 = parseInt(num_1Ref.current.value);
+    let no2 = parseInt(num_2Ref.current.value);
+    setSum(no1 + no2);
+  };
+
+  return (
+    <div>
+      <label>
+        Enter the first number :
+        <input type='number' ref={num_1Ref} />
+      </label>
+      <br></br>
+      <br></br>
+      <label>
+        Enter the second number :
+        <input type='number' ref={num_2Ref} />
+      </label>
+      <br></br>
+      <button onClick={addNumbers}>Add Numbers</button>
+      <br></br>
+      <p hidden={Sum === 0 ? true : false}>Sum of input numbers: {Sum}</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<Sum />, document.getElementById("root"));
